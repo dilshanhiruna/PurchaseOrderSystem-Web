51fb9c9fe4fe5320626fd0e5c2c36c15f1ec6b3c
dilshanhiruna <dilshan.hirunaa@gmail.com>
2021-09-30 18:53:37 +0530

    Add Router Links

--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,49 @@
 import React from "react";
 import app from "./base";
-import '../purchaseOrder_Components/PurchaseOrder1.css'
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <>
-      <button onClick={() => app.auth().signOut()}>Sign out</button>
-    
       <div className="sidenav">
-        <a href="#"><i class="fa fa-fw fa-home"></i>  Dashboard</a>
-        <a href="#"><i class="fa fa-fw fa-phone"></i>  Contacts</a>
-        <a href="#"><i class="fa fa-fw fa-user-circle-o"></i>  Admin</a>
-        <a href="/purchaseorder"><i class="fa fa-fw fa-shopping-cart"></i>  Purchase Orders</a>
+        <Link to="/">
+          <i class="fa fa-fw fa-home"></i> Dashboard
+        </Link>
+        <Link href="/">
+          <i class="fa fa-fw fa-phone"></i> Contacts
+        </Link>
+        <Link href="/">
+          <i class="fa fa-fw fa-user-circle-o"></i> Admin
+        </Link>
+        <Link to="/purchaseorder">
+          <i class="fa fa-fw fa-shopping-cart"></i> Purchase Orders
+        </Link>
 
-        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
 
-        <a href="#"><i class="fa fa-fw fa-sign-out"></i>  Logout</a>
+        <button onClick={() => app.auth().signOut()}>Sign out</button>
       </div>
 
       <div className="puchaseOrdermain">
-          <h1>Home</h1>
+        <h1>Home</h1>
       </div>
     </>
   );
--- src/purchaseOrder_Components/purchaseOrder1.js
+++ src/purchaseOrder_Components/purchaseOrder1.js
@@ -1,69 +1,95 @@
-import React,{useState,useEffect} from 'react'
-import {Link} from "react-router-dom"
-import firebase from '../base'
-import './PurchaseOrder1.css'
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import firebase from "../base";
+import "./purchaseOrder1.css";
 
 function PurchaseOrder1() {
+  const [order, setOrder] = useState([true]);
 
-    const [order, setOrder] = useState([true])
- 
-    useEffect(() => {
-        const fetchData = async() => {
-            const db = firebase.firestore()
-            const data = await db.collection("order").get()
-            setOrder(data.docs.map(doc => doc.data()))
-        } 
-        fetchData()
-    }, [])
+  useEffect(() => {
+    const fetchData = async () => {
+      const db = firebase.firestore();
+      const data = await db.collection("order").get();
+      setOrder(data.docs.map((doc) => doc.data()));
+    };
+    fetchData();
+  }, []);
 
-    return (
-        <div>
-            <div className="sidenav">
-                <a href="/"><i class="fa fa-fw fa-home"></i>  Dashboard</a>
-                <a href="#"><i class="fa fa-fw fa-phone"></i>  Contacts</a>
-                <a href="#"><i class="fa fa-fw fa-user-circle-o"></i>  Admin</a>
-                <a href="#"><i class="fa fa-fw fa-shopping-cart"></i>  Purchase Orders</a>
-                <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-                <a href="#"><i class="fa fa-fw fa-sign-out"></i>  Logout</a>
-            </div>
-            <div className="puchaseOrdermain">
-                <div className="purchaseOrderTOP">
-                    <h5>Purchase Order</h5>
-
-                    
-                </div>
-                <br/>
-                <table border="1" id="myt">
-                <tr>
-                    <th>Order ID</th>
-                    <th>Purchase_date</th>
-                    <th>Required_date</th>
-                    <th>order_status</th>
-                    <th>site_Name</th>
-                    <th>supplierName</th>
-                    <th>Total Price</th>
-                    <th>comments</th>
-                    <th>Action</th>
-                </tr>
-                {order && order.map(o => (
-                    <tr>
-                    <td key={o.OrderID}> {o.OrderID} </td>
-                    <td key={o.Purchase_date}> {o.Purchase_date}</td>
-                    <td key={o.Required_date}> {o.Required_date}</td>
-                    <td key={o.order_status}> {o.order_status}</td>
-                    <td key={o.site_Name}> {o.site_Name}</td>
-                    <td key={o.supplierName}> {o.supplierName}</td>
-                    <td key={o.total_price}> {o.total_price}</td>
-                    <td key={o.comments}> {o.comments}</td>
-                    <td><button className="purchaseOrder_viewBtn">View</button></td>
-                    </tr>
-                ))}
-            </table>
-                
-            </div>
-            
+  return (
+    <div>
+      <div className="sidenav">
+        <Link to="/">
+          <i class="fa fa-fw fa-home"></i> Dashboard
+        </Link>
+        <Link to="/">
+          <i class="fa fa-fw fa-phone"></i> Contacts
+        </Link>
+        <Link to="/">
+          <i class="fa fa-fw fa-user-circle-o"></i> Admin
+        </Link>
+        <Link to="/">
+          <i class="fa fa-fw fa-shopping-cart"></i> Purchase Orders
+        </Link>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <a href="#">
+          <i class="fa fa-fw fa-sign-out"></i> Logout
+        </a>
+      </div>
+      <div className="puchaseOrdermain">
+        <div className="purchaseOrderTOP">
+          <h5>Purchase Order</h5>
         </div>
-    )
+        <br />
+        <table border="1" id="myt">
+          <tr>
+            <th>Order ID</th>
+            <th>Purchase_date</th>
+            <th>Required_date</th>
+            <th>order_status</th>
+            <th>site_Name</th>
+            <th>supplierName</th>
+            <th>Total Price</th>
+            <th>comments</th>
+            <th>Action</th>
+          </tr>
+          {order &&
+            order.map((o) => (
+              <tr>
+                <td key={o.OrderID}> {o.OrderID} </td>
+                <td key={o.Purchase_date}> {o.Purchase_date}</td>
+                <td key={o.Required_date}> {o.Required_date}</td>
+                <td key={o.order_status}> {o.order_status}</td>
+                <td key={o.site_Name}> {o.site_Name}</td>
+                <td key={o.supplierName}> {o.supplierName}</td>
+                <td key={o.total_price}> {o.total_price}</td>
+                <td key={o.comments}> {o.comments}</td>
+                <td>
+                  <button className="purchaseOrder_viewBtn">View</button>
+                </td>
+              </tr>
+            ))}
+        </table>
+      </div>
+    </div>
+  );
 }
 
-export default PurchaseOrder1
+export default PurchaseOrder1;
