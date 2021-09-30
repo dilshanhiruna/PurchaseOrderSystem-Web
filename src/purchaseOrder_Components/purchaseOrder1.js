ad45d8417f91e87c06254bf6a793ac7a38a6a66c
dilshanhiruna <dilshan.hirunaa@gmail.com>
2021-09-30 19:59:05 +0530

    Table Design Improvement

--- src/purchaseOrder_Components/purchaseOrder1.js
+++ src/purchaseOrder_Components/purchaseOrder1.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import firebase from "../base";
-import "./purchaseOrder1.css";
+import "./PurchaseOrder.css";
 
 function PurchaseOrder1() {
   const [order, setOrder] = useState([true]);
@@ -17,39 +17,43 @@ function PurchaseOrder1() {
 
   return (
     <div>
-      <div className="puchaseOrdermain">
-        <div className="purchaseOrderTOP">
-          <h5>Purchase Order</h5>
-        </div>
-        <br />
-        <table border="1" id="myt">
-          <tr>
-            <th>Order ID</th>
-            <th>Purchase_date</th>
-            <th>Required_date</th>
-            <th>order_status</th>
-            <th>site_Name</th>
-            <th>supplierName</th>
-            <th>Total Price</th>
-            <th>comments</th>
-            <th>Action</th>
-          </tr>
-          {order &&
-            order.map((o) => (
-              <tr>
-                <td key={o.OrderID}> {o.OrderID} </td>
-                <td key={o.Purchase_date}> {o.Purchase_date}</td>
-                <td key={o.Required_date}> {o.Required_date}</td>
-                <td key={o.order_status}> {o.order_status}</td>
-                <td key={o.site_Name}> {o.site_Name}</td>
-                <td key={o.supplierName}> {o.supplierName}</td>
-                <td key={o.total_price}> {o.total_price}</td>
-                <td key={o.comments}> {o.comments}</td>
-                <td>
-                  <button className="purchaseOrder_viewBtn">View</button>
-                </td>
-              </tr>
-            ))}
+      <div className="header-box">
+        <div>Purchase Order</div>
+      </div>
+
+      <div className="content-box-list">
+        <table>
+          <thead>
+            <tr>
+              <th>Order ID</th>
+              <th>Purchase Date</th>
+              <th>Required Date</th>
+              <th>Order Status</th>
+              <th>Site Name</th>
+              <th>Supplier Name</th>
+              <th>Total Price</th>
+              <th>Comments</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {order &&
+              order.map((o) => (
+                <tr>
+                  <td key={o.OrderID}> {o.OrderID} </td>
+                  <td key={o.Purchase_date}> {o.Purchase_date}</td>
+                  <td key={o.Required_date}> {o.Required_date}</td>
+                  <td key={o.order_status}> {o.order_status}</td>
+                  <td key={o.site_Name}> {o.site_Name}</td>
+                  <td key={o.supplierName}> {o.supplierName}</td>
+                  <td key={o.total_price}> {o.total_price}</td>
+                  <td key={o.comments}> {o.comments}</td>
+                  <td>
+                    <button className="purchaseOrder_viewBtn">View</button>
+                  </td>
+                </tr>
+              ))}
+          </tbody>
         </table>
       </div>
     </div>
