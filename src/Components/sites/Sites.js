ae0abe252b89fe8153d94f6f1a095e5e2bfc9622
99hash63 <thkottegoda@gmail.com>
2021-10-02 16:46:37 +0530

    created add site and delete site functions

--- src/Components/sites/Sites.js
+++ src/Components/sites/Sites.js
@@ -3,44 +3,41 @@ import ViewSites from './ViewSites';
 import AddSite from './AddSite';
 import React, { useState, useEffect } from 'react';
 import firebase from '../../base';
+import { v4 as uuidv4 } from 'uuid';
+import EditSite from './EditSite';
 
 export default function Sites() {
 	return (
 		<Router>
 			<div>
-				<nav>
-					<ul>
-						{/* <li>
-							<Link to="/sites">View</Link>
-						</li>
-						<li>
-							<Link to="/addSite">Add</Link>
-						</li> */}
-						<Link to="/addSite">
-							<button type="button">Add Site</button>
-						</Link>
-					</ul>
-				</nav>
-
+				<Link to="/sites">
+					<button type="button">View Sites</button>
+				</Link>
+				<Link to="/addSite">
+					<button type="button">Add Site</button>
+				</Link>
 				{/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
+		renders the first one that matches the current URL. */}
 				<Switch>
-					<Route path="/sites">
+					<Route exact path="/sites">
 						<V />
 					</Route>
-					<Route path="/addSite">
+					<Route exact path="/addSite">
 						<A />
+					</Route>
+					<Route exact path="/editSite">
+						<EditSite></EditSite>
 					</Route>
 				</Switch>
 			</div>
 		</Router>
 	);
-}
 
-function V() {
-	return <ViewSites></ViewSites>;
-}
+	function V() {
+		return <ViewSites></ViewSites>;
+	}
 
-function A() {
-	return <AddSite></AddSite>;
+	function A() {
+		return <AddSite></AddSite>;
+	}
 }
--- src/Components/sites/ViewSites.js
+++ src/Components/sites/ViewSites.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 import firebase from '../../base';
 
-export default function ViewSites() {
-	const [sites, setSites] = useState([]);
-	const [loading, setLoading] = useState(false);
+export default function ViewSites(props) {
+	// const sites = props.sites;
+	// const loading = props.loading;
 
 	const ref = firebase.firestore().collection('sites');
-	console.log(ref);
+	const [sites, setSites] = useState([]);
+	const [loading, setLoading] = useState(false);
 
 	//The function to retreive sites data from the database
 	function getSites() {
@@ -25,45 +28,72 @@ export default function ViewSites() {
 		getSites();
 	}, []);
 
-	//Show a h1 tag saying loading if loading is true
+	//delete function
+	function deleteSite(site) {
+		ref
+			.doc(site.id)
+			.delete()
+			.catch((err) => {
+				console.log(err);
+			});
+	}
+
+	//edit function
+	function editSite(updatedSite) {
+		ref
+			.doc(updatedSite.id)
+			.update(updatedSite)
+			.catch((err) => {
+				console.log(err);
+			});
+	}
+
+	// Show a h1 tag saying loading if loading is true
 	if (loading) {
 		return <h1>Loading...</h1>;
 	}
-
-	//show table if data is avaialble
 	return (
 		<div>
-			<div className="header-box">
-				<div>Sites</div>
-			</div>
+			<div>
+				<div className="header-box">
+					<div>Sites</div>
+				</div>
 
-			<div className="content-box-list" style={{ width: '80%' }}>
-				<table>
-					<thead>
-						<tr>
-							<th>Site Name</th>
-							<th>Location</th>
-							<th>Budget</th>
-							<th>Auto Approval Limit</th>
-							<th></th>
-						</tr>
-					</thead>
-					<tbody>
-						{sites &&
-							sites.map((site) => (
-								<tr>
-									<td key={site.name}> {site.name}</td>
-									<td key={site.location}> {site.location}</td>
-									<td key={site.budget}> {site.budget}</td>
-									<td key={site.limit}> {site.limit}</td>
-									<td>
-										<button className="purchaseOrder_viewBtn">Edit</button>
-										<button className="purchaseOrder_viewBtn">Delete</button>
-									</td>
-								</tr>
-							))}
-					</tbody>
-				</table>
+				<div className="content-box-list">
+					<table>
+						<thead>
+							<tr>
+								<th>Site Name</th>
+								<th>Location</th>
+								<th>Budget</th>
+								<th>Auto Approval Limit</th>
+								<th></th>
+							</tr>
+						</thead>
+						<tbody>
+							{sites &&
+								sites.map((site) => (
+									<tr>
+										<td> {site.name}</td>
+										<td> {site.location}</td>
+										<td> {site.budget}</td>
+										<td> {site.limit}</td>
+										<td>
+											<Link to="/editSite" params={{ testvalue: 'hello' }}>
+												<button className="purchaseOrder_viewBtn">Edit</button>
+											</Link>
+											<button
+												onClick={() => deleteSite(site)}
+												className="purchaseOrder_viewBtn"
+											>
+												Delete
+											</button>
+										</td>
+									</tr>
+								))}
+						</tbody>
+					</table>
+				</div>
 			</div>
 		</div>
 	);
