879affbae786419061620666c8f788a695295430
thiyuni <thiyunithilakarathne9848@gmail.com>
2021-10-04 23:15:02 +0530

    Add comments to rootReducer.js

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,6 +2,7 @@ import {combineReducers} from "redux";
 import supplierReducer from "./reducer";
 
 const rootReducer = combineReducers({
+    //To access all reducer states with the help of the "data" keyword
     data: supplierReducer
 });
 
