b8e86a613636172551e19aed4935011816031501
99hash63 <thkottegoda@gmail.com>
2021-10-16 09:14:03 +0530

    could not complete test cases

--- src/Sites.test.js
+++ src/Sites.test.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import Sites from './Sites';
+import { addSite } from './reactDB/SiteDBUtil';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('SitesComponent', () => {
-	it('should be true', () => {
-		const foo = true;
-		expect(foo).toBe(true);
+	// it('should show text', () => {
+	// 	const wrapper = shallow(<SiteDBUtil />);
+	// 	const text = wrapper.find('div div');
+	// 	expect(text.text()).toBe('hey');
+	// 	// expect(test).toBe(true);
+	// });
+
+	it('should return true', () => {
+		const val = addSite();
+		expect(val).toEqual(true);
 	});
 });
---
+++ src/reactDB/SiteDBUtil.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+
+function addSite() {
+	return true;
+}
+
+function getSite() {
+	return true;
+}
+export default { addSite, getSite };
